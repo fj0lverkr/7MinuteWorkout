c1b08956b5fd285a7588aab5b977375d34541b66
Nils Nahooy <nils.nahooy@live.com>
2022-09-15 11:00:13 +0200

    Main Activity initial lay-out

--- app/src/main/java/com/nilsnahooy/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,16 @@ package com.nilsnahooy.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton: FrameLayout = findViewById(R.id.fl_button_wrapper)
+        flStartButton.setOnClickListener{
+
+        }
     }
 }
