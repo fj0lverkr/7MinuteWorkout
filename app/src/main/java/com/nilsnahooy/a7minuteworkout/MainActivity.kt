dcfc400c2303543fbd2aaf298182a1bdb48495f1
Nils Nahooy <nils.nahooy@live.com>
2022-09-15 11:24:17 +0200

    Added viewBinding for ActivityMain

--- app/src/main/java/com/nilsnahooy/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.nilsnahooy.a7minuteworkout
 
+/* NOTE ON USING VIEWBINDING
+        - Faster, more efficient during compilation, safer, more concise in code.
+        - becomes available after setting
+                buildFeatures{
+                    viewBinding true
+                }
+          in the android section of build.gradle
+        - for usage, see code below (var b?)
+        - make sure to set the binding back to null in onDestroy()
+     */
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.nilsnahooy.a7minuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var b:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.fl_button_wrapper)
-        flStartButton.setOnClickListener{
+        b?.flButtonWrapper?.setOnClickListener {
 
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        b = null
     }
 }
