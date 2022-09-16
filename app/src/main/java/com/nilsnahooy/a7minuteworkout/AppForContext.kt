a299200a17650fe9194155a7d955322042b615b3
Nils Nahooy <nils.nahooy@live.com>
2022-09-16 16:52:48 +0200

    Created static Application context.

---
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/AppForContext.kt
@@ -0,0 +1,17 @@
+package com.nilsnahooy.a7minuteworkout
+
+import android.app.Application
+import android.content.res.Resources
+
+class AppForContext : Application() {
+
+   companion object{
+       lateinit var res: Resources private set
+   }
+
+    override fun onCreate() {
+        super.onCreate()
+        res = resources
+    }
+
+}
