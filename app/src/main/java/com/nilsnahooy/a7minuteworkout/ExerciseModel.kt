211d95c6d06ecbc985b13bf4e6e8e4a0cf7efec8
Nils Nahooy <nils.nahooy@live.com>
2022-09-16 10:08:05 +0200

    Added Exercise model.

---
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/ExerciseModel.kt
@@ -0,0 +1,47 @@
+package com.nilsnahooy.a7minuteworkout
+
+class ExerciseModel(
+    private var id: Int,
+    private var name: String,
+    private var duration: Long,
+    private var imageRes: Int,
+    private var isCompleted: Boolean,
+    private var isSelected: Boolean
+) {
+    fun getId(): Int{
+        return id
+    }
+    fun setId(id:Int){
+        this.id = id
+    }
+    fun getName(): String{
+        return name
+    }
+    fun setName(name:String){
+        this.name = name
+    }
+    fun getDuration():Long {
+        return duration
+    }
+    fun setDuration(duration: Long){
+        this.duration = duration
+    }
+    fun getImageRes():Int{
+        return imageRes
+    }
+    fun setImageRes(imageRes: Int){
+        this.imageRes = imageRes
+    }
+    fun getIsCompleted():Boolean{
+        return isCompleted
+    }
+    fun setIsCompleted(isCompleted: Boolean){
+        this.isCompleted = isCompleted
+    }
+    fun getIsSelected(): Boolean{
+        return isSelected
+    }
+    fun setIsSelected(isSelected: Boolean){
+        this.isSelected = isSelected
+    }
+}
