996ee7460af6d3d104bc9d8de1db1c19fc7f41c2
Nils Nahooy <nils.nahooy@live.com>
2022-09-16 16:53:19 +0200

    Moved exercise names to string resources.

--- app/src/main/java/com/nilsnahooy/a7minuteworkout/Constants.kt
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/Constants.kt
@@ -5,52 +5,58 @@ object Constants {
         val exerciseList = ArrayList<ExerciseModel>()
 
         exerciseList.add(
-            ExerciseModel(1,"abdominal crunch", 30000,
-                R.drawable.ic_abdominal_crunch, isCompleted = false, isSelected = false))
+            ExerciseModel(1, AppForContext.res.getString(R.string.ex_abdominal_crunch),
+                30000, R.drawable.ic_abdominal_crunch, isCompleted = false,
+                isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(2,"high knees running in place", 30000,
+            ExerciseModel(2, AppForContext.res.getString(
+                R.string.ex_high_knees_running_in_place), 30000,
                 R.drawable.ic_high_knees_running_in_place, isCompleted = false, isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(3,"jumping jacks", 30000,
-                R.drawable.ic_jumping_jacks, isCompleted = false, isSelected = false))
+            ExerciseModel(3, AppForContext.res.getString(R.string.ex_jumping_jacks),
+                30000, R.drawable.ic_jumping_jacks, isCompleted = false,
+                isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(4,"lunge", 30000,
+            ExerciseModel(4, AppForContext.res.getString(R.string.ex_lunge), 30000,
                 R.drawable.ic_lunge, isCompleted = false, isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(5,"plank", 30000,
+            ExerciseModel(5, AppForContext.res.getString(R.string.ex_plank), 30000,
                 R.drawable.ic_plank, isCompleted = false, isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(6,"push-up", 30000,
-                R.drawable.ic_push_up, isCompleted = false, isSelected = false))
+            ExerciseModel(6, AppForContext.res.getString(R.string.ex_push_up),
+                30000, R.drawable.ic_push_up, isCompleted = false, isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(7,"push-up and rotation", 30000,
-                R.drawable.ic_push_up_and_rotation, isCompleted = false, isSelected = false))
+            ExerciseModel(7, AppForContext.res.getString(R.string.ex_push_up_rotation),
+                30000, R.drawable.ic_push_up_and_rotation, isCompleted = false,
+                isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(8,"side plank", 30000,
-                R.drawable.ic_side_plank, isCompleted = false, isSelected = false))
+            ExerciseModel(8, AppForContext.res.getString(R.string.ex_side_plank),
+                30000, R.drawable.ic_side_plank, isCompleted = false, isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(9,"squat", 30000,
+            ExerciseModel(9, AppForContext.res.getString(R.string.ex_squat), 30000,
                 R.drawable.ic_squat, isCompleted = false, isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(10,"step up onto chair", 30000,
-                R.drawable.ic_step_up_onto_chair, isCompleted = false, isSelected = false))
+            ExerciseModel(10, AppForContext.res.getString(R.string.ex_step_up_onto_chair),
+                30000, R.drawable.ic_step_up_onto_chair, isCompleted = false,
+                isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(11,"triceps dip on chair", 30000,
-                R.drawable.ic_triceps_dip_on_chair, isCompleted = false, isSelected = false))
+            ExerciseModel(11, AppForContext.res.getString(R.string.ex_triceps_dip_on_chair),
+                30000, R.drawable.ic_triceps_dip_on_chair, isCompleted = false,
+                isSelected = false))
 
         exerciseList.add(
-            ExerciseModel(12,"wall sit", 30000,
-                R.drawable.ic_wall_sit, isCompleted = false, isSelected = false))
+            ExerciseModel(12, AppForContext.res.getString(R.string.ex_wall_sit),
+                30000, R.drawable.ic_wall_sit, isCompleted = false, isSelected = false))
 
         return exerciseList.shuffled() as ArrayList<ExerciseModel>
     }
