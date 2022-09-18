e10b0f9ad820cc5c7e11ae606326385a0318d19a
Nils Nahooy <nils.nahooy@live.com>
2022-09-18 19:56:31 +0200

    Setting up layout for BMI Activity.

--- app/src/main/java/com/nilsnahooy/a7minuteworkout/BmiActivity.kt
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/BmiActivity.kt
@@ -2,10 +2,28 @@ package com.nilsnahooy.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nilsnahooy.a7minuteworkout.databinding.ActivityBmiBinding
 
 class BmiActivity : AppCompatActivity() {
+    private var b : ActivityBmiBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_bmi)
+        b = ActivityBmiBinding.inflate(layoutInflater)
+        setContentView(b?.root)
+
+        setSupportActionBar(b?.tbBmi)
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+        b?.tbBmi?.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        b = null
     }
 }
