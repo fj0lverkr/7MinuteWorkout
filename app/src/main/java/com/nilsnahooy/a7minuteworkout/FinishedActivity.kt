66d4c306dd6e131745711a4eefe8de1191127a15
Nils Nahooy <nils.nahooy@live.com>
2022-09-18 14:50:43 +0200

    Add finished activity

---
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/FinishedActivity.kt
@@ -0,0 +1,28 @@
+package com.nilsnahooy.a7minuteworkout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nilsnahooy.a7minuteworkout.databinding.ActivityFinishedBinding
+
+class FinishedActivity : AppCompatActivity() {
+    private var b: ActivityFinishedBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        b = ActivityFinishedBinding.inflate(layoutInflater)
+        setContentView(b?.root)
+
+        setSupportActionBar(b?.tbExercise)
+
+        b?.btnFinish?.setOnClickListener {
+            val intent =  Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        b = null
+    }
+}
