2645b4ef1bbceb17f1dab777b6b5213bc3ac5134
Nils Nahooy <nils.nahooy@live.com>
2022-09-24 11:16:36 +0200

    Removed unneccesary code

--- app/src/main/java/com/nilsnahooy/a7minuteworkout/workoutHistory/WorkoutDatabase.kt
+++ app/src/main/java/com/nilsnahooy/a7minuteworkout/workoutHistory/WorkoutDatabase.kt
@@ -4,11 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.nilsnahooy.a7minuteworkout.utils.Converters
 
 @Database(entities = [WorkoutEntity::class], version = 1)
-@TypeConverters(Converters::class)
 abstract class WorkoutDatabase: RoomDatabase() {
     abstract fun workoutDao(): WorkoutDao
 
